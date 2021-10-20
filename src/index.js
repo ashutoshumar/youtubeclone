a0a60566c41a1446158e0dadfe0f0be7cb88fdfb
ashutoshumar <viraj28300@gmail.com>
2021-10-20 12:01:21 +0530

    second

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from "react";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap-grid.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-lazy-load-image-component/src/effects/blur.css'
 import "./_base.scss"
 import {Provider} from "react-redux";
